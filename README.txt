e335f485f8252351dc452a60d7a84a3b29c59eed
Stuart Stevenson <stuartjackstevenson@gmail.com>
2013-05-04 17:32:49 +0100

    rename to default

--- README.txt
+++ README.txt
@@ -2,6 +2,3 @@ currently this project is a simple hello world spring 3 mvc and maven app.
 
 It now includes sitemesh3.
 
-It deploys using jetty and is built using travis-ci.org (Note: travis is currently disabled until this app does something interesting)
-
-
--- src/main/java/org/stu/arch/DefaultSiteMeshFilter.java
+++ src/main/java/org/stu/arch/DefaultSiteMeshFilter.java
@@ -3,7 +3,7 @@ package org.stu.arch;
 import org.sitemesh.builder.SiteMeshFilterBuilder;
 import org.sitemesh.config.ConfigurableSiteMeshFilter;
 
-public class RmBeerRunSiteMeshFilter extends ConfigurableSiteMeshFilter {
+public class DefaultSiteMeshFilter extends ConfigurableSiteMeshFilter {
     @Override
     protected void applyCustomConfiguration(SiteMeshFilterBuilder siteMeshFilterBuilder) {
         siteMeshFilterBuilder.addDecoratorPath("/*","/WEB-INF/jsp/decorators/default.jsp");
