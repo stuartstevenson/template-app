ea4c573ce8e735710c36b87d9fd692ed60285ecc
stuartstevenson <stuartjackstevenson@gmail.com>
2013-07-02 16:51:01 +0100

    test failure test

--- src/test/java/org/stu/web/HelloControllerTest.java
+++ src/test/java/org/stu/web/HelloControllerTest.java
@@ -3,11 +3,13 @@ package org.stu.web;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
 
 public class HelloControllerTest {
     @Test
     public void testShowHello() {
         HelloController helloController = new HelloController();
         assertEquals("hello", helloController.showHello());
+		fail();
     }
 }
