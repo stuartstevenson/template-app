74147a8c045197ef7f5f45b166e7581caa8f8267
Stuart Stevenson <stuartjackstevenson@gmail.com>
2013-07-02 20:27:12 +0100

    integrationtest

---
+++ src/test/java/org/stu/arch/DefaultSiteMeshFilterIT.java
@@ -0,0 +1,12 @@
+package org.stu.arch;
+
+import org.junit.Test;
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+
+public class DefaultSiteMeshFilterIT {
+    @Test
+    public void testCustomConfiguration() {
+        SiteMeshFilterBuilder siteMeshFilterBuilder = new SiteMeshFilterBuilder();
+        new DefaultSiteMeshFilter().applyCustomConfiguration(siteMeshFilterBuilder);
+    }
+}
--- src/test/java/org/stu/web/HelloControllerTest.java
+++ src/test/java/org/stu/web/HelloControllerTest.java
@@ -3,7 +3,6 @@ package org.stu.web;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.fail;
 
 public class HelloControllerTest {
     @Test
