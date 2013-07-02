255effa3a92d01f88143d111b06ac3527fa71e19
Stuart Stevenson <stuartjackstevenson@gmail.com>
2013-07-02 21:33:16 +0100

    integrationtest

--- src/test/java/org/stu/arch/DefaultSiteMeshFilterIT.java
+++ src/test/java/org/stu/arch/DefaultSiteMeshFilterIT.java
@@ -3,10 +3,13 @@ package org.stu.arch;
 import org.junit.Test;
 import org.sitemesh.builder.SiteMeshFilterBuilder;
 
+import static org.junit.Assert.fail;
+
 public class DefaultSiteMeshFilterIT{
     @Test
     public void testCustomConfiguration() {
         SiteMeshFilterBuilder siteMeshFilterBuilder = new SiteMeshFilterBuilder();
         new DefaultSiteMeshFilter().applyCustomConfiguration(siteMeshFilterBuilder);
+        fail();
     }
 }
